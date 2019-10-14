c207a589b603ee6fbcedcb79e52693749cc407a1
mellownightpirate <54673937+mellownightpirate@users.noreply.github.com>
2019-10-14 19:36:25 +0100

    stretch goals complete

--- assignments/objects.js
+++ assignments/objects.js
@@ -105,25 +105,39 @@ console.log(Antonietta.multiplyNums(3,4))
 const parent = {
 name: "Susan",
 age: 70,
-child: {name: "George",
-age: 50,
-grandchild: {name: "Sam", age: 30},
+speak: function () {
+  return "Hi, my name is " + this.name;
 },
-speak: function() {
-  console.log(`Hi! My name is ${this.name}`);
+child: {
+  name: "George",
+  age: 50,
+  speak: function () {
+    return "Hi, my name is " + this.name;
+  },
+  grandchild: {
+    name: "Sam",
+    age: 30,
+    speak: function () {
+      return "Hi, my name is " +this.name;
+    }
+  }
 }
 }
-
 
 // Log the parent object's name
 console.log(parent.name);
 
 // Log the child's age
+console.log(parent.child.age);
 
 // Log the name and age of the grandchild
+console.log (parent.child.grandchild.name, parent.child.grandchild.age);
 
 // Have the parent speak
+console.log (parent.speak())
 
 // Have the child speak
+console.log (parent.child.speak())
 
 // Have the grandchild speak
+console.log (parent.child.grandchild.speak())
